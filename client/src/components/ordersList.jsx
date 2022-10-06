68bb225636f74efcc124469addd56ca4c194072e
Valeriy Harlamov <valera545@gmail.com>
2022-10-06 18:28:33 +0300

    refactoring 06.10 2

--- client/src/components/ordersList.jsx
+++ client/src/components/ordersList.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { getOrdersList } from '../store/orders'
 
 const OrdersList = () => {
+	const orders = useSelector(getOrdersList())
+
+	const columns = {}
+
 	return <h1>OrdersList</h1>
 }
 
