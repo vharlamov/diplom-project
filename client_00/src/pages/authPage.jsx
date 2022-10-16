6e827a5d661ec2ebf470e938d6d12eb13312614a
Valeriy Harlamov <valera545@gmail.com>
2022-10-16 14:29:49 +0300

    fix endpoint

--- client_00/src/pages/authPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const AuthPage = () => {
-	return <h1>Логин/Регистрация</h1>
-}
-
-export default AuthPage
--- client_00/src/services/shCart.service.js
+++
@@ -1,1 +0,0 @@
-import localStorageService from './localStorage.service'
