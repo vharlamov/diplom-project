15caae59a23d11748d2e1f22d2d2bbb8f3e74389
Valeriy Harlamov <valera545@gmail.com>
2022-12-04 13:33:20 +0300

    fozzy deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 const config = {
 	local: 'http://localhost:8080/',
 	heroku: 'https://eshop-diplom.herokuapp.com/',
+	fozzy: 'http://23.111.202.40/',
 }
 
-export default config.local
+export default config.fozzy
